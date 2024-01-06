9042ddf140430a67a8c10140a11b111078586dd6
nirtendra <nirtendrayadav12@gmail.com>
2024-01-06 20:38:11 +1100

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName ="bob"
+
+const leastFavoriteCustomer = "hello"
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+  return customerName
+
+}
+
+
+
+
+
+function setBestCustomer() {
+     return bestCustomer = "not bob"
+}
+
+
+
+
+
+function overwriteBestCustomer() {
+  return  bestCustomer = "maybe bob"
+}
+
+
+
+function changeLeastFavoriteCustomer(){
+   return  leastFavoriteCustomer = "nello"
+}
+
+
